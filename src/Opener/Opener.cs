4c2a0cc07562039b3f3c206a715230fc60eafb48
vytcka <c4002911@newcastle.ac.uk>
2025-07-21 14:09:13 -0800

    added functionality for code where it now reads the domains of the file, though mroe modular code would be needed.

---
+++ src/Opener/Opener.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+public class Opener
+{
+
+    List<string> domains = new List<string>();
+    public List<string> fileOpen()
+    {
+        
+        string line;
+        try
+        {
+            StreamReader sr = new StreamReader("../../../../../domain.txt");
+            line = sr.ReadLine();
+            domains.Add(line);
+            while (line != null)
+            {
+
+                line = sr.ReadLine();
+                domains.Add(line);
+            }
+            sr.Close();
+            
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("The error: " + e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Finished");
+        }
+        return domains;
+    }
+
+
+
+    public static void Main()
+    {
+        Opener s = new Opener();
+        s.fileOpen();
+
+        foreach (string name in s.domains)
+        {
+            Console.WriteLine(name);
+        }
+
+    }
+
+}
